89da1ea24149a0606fc7c4e2bf274f6e88aff815
Prasana Ravikumar <prasana.ravikumar@yahoo.com>
2026-02-20 13:32:55 -0500

    Update script

--- scripts/gitlogs.ps1
+++ scripts/gitlogs.ps1
@@ -1,11 +1,35 @@
-# Set your date filters (format: yyyy-MM-dd)
-$since = "2025-01-01"
-$until = "2026-02-20"
+param(
+    [string]$RepoPath = ".",
+    [string]$Since = "",
+    [string]$Until = ""
+)
 
-$authors = git log --since="$since" --until="$until" --pretty=format:"%an <%ae>" | Sort-Object | Get-Unique
+# Change to the repo directory
+Set-Location $RepoPath
+
+# Get repo name from folder
+$repoName = Split-Path -Leaf (Get-Location)
+
+# Get timestamp
+$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+
+# Output file name
+$outputFile = "$repoName-gitlog-$timestamp.txt"
+
+
+# Build date filter args as array
+$dateArgs = @()
+if ($Since -ne "") { $dateArgs += "--since=$Since" }
+if ($Until -ne "") { $dateArgs += "--until=$Until" }
+
+# Get unique authors
+$authors = git log @dateArgs --pretty=format:"%an <%ae>" | Sort-Object | Get-Unique
+
+# Prepare output
+$output = @()
 foreach ($author in $authors) {
-    Write-Host $author
-    $stats = git log --author="$author" --since="$since" --until="$until" --pretty=tformat: --numstat | 
+    $output += $author
+    $stats = git log --author="$author" @dateArgs --pretty=tformat: --numstat |
         ForEach-Object {
             $fields = $_ -split "\s+"
             if ($fields.Length -ge 2) {
@@ -18,5 +42,11 @@ foreach ($author in $authors) {
         $added += $stat[0]
         $deleted += $stat[1]
     }
-    Write-Host "Added: $added Deleted: $deleted Net: $(($added - $deleted))"
+    $output += "Added: $added Deleted: $deleted Net: $(($added - $deleted))"
+    $output += ""
 }
+
+# Write to file
+$output | Set-Content $outputFile
+
+Write-Host "Output written to $outputFile"
